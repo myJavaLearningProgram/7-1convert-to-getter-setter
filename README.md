d29f381653961eb2b6e2471b9598203e2de41d52
Ories <tellt1@126.com>
2022-05-18 09:22:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Java面向对象初步练习：将成员改写为getter/setter
+
+请在`Cat`类中，将原先的`public`成员改写成`getter`/`setter`方法
+
+在提交Pull Request之前，你应当在本地确保所有代码已经编译通过，并且通过了测试(`mvn clean verify`)
+
+-----
+注意！我们只允许你修改以下文件，对其他文件的修改会被拒绝：
+- src/main/java/com/github/hcsp/encapsulation/Cat.java
+-----
+
+
+
---
+++ src/test/java/com/github/hcsp/encapsulation2/CatTest.java
@@ -0,0 +1,34 @@
+package com.github.hcsp.encapsulation2;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Random;
+import java.util.UUID;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CatTest {
+    @Test
+    public void hasGettersAndSetters() {
+        String name = UUID.randomUUID().toString();
+        int age = new Random().nextInt(10);
+        com.github.hcsp.encapsulation.Cat cat = new com.github.hcsp.encapsulation.Cat();
+
+        cat.setName(name);
+        cat.setAge(age);
+        cat.setCute(true);
+
+        Assertions.assertEquals(name, cat.getName());
+        Assertions.assertEquals(age, cat.getAge());
+        Assertions.assertTrue(cat.isCute());
+    }
+
+    @Test
+    public void allFieldsArePrivate() {
+        Assertions.assertTrue(
+                Stream.of(com.github.hcsp.encapsulation.Cat.class.getDeclaredFields())
+                        .map(Field::getModifiers)
+                        .allMatch(Modifier::isPrivate));
+    }
+}
